0d199a1526ae1de5f2863f07958fe250913adaaa
ValeriaPFR <ValeriaPFR@users.noreply.github.com>
2024-05-04 16:36:35 -0400

    Add challenge files

--- manipulacion_transaccionalidad.sql
+++ manipulacion_transaccionalidad.sql
@@ -2,10 +2,26 @@
 CREATE DATABASE desafio_valeria_farinia_002;
 
 --B.- Conectarse a la base de datos creada:
-\c desafio_valeria_farinia_002;
-
+\c desafio_valeria_farinia_002; 
 --Crear la tabla e insertar los registros:
 --COPIAR LOS REGISTROS DESDE EL ARCHIVO DEL DESAFÍO
+CREATE TABLE IF NOT EXISTS inscritos(cantidad INT, fecha DATE, fuente VARCHAR); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 44, '01/01/2021', 'Blog' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 56, '01/01/2021', 'Página' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 39, '01/02/2021', 'Blog' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 81, '01/02/2021', 'Página' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 12, '01/03/2021', 'Blog' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 91, '01/03/2021', 'Página' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 48, '01/04/2021', 'Blog' );
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 45, '01/04/2021', 'Página' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 55, '01/05/2021', 'Blog' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 33, '01/05/2021', 'Página' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 18, '01/06/2021', 'Blog' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 12, '01/06/2021', 'Página' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 34, '01/07/2021', 'Blog' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 24, '01/07/2021', 'Página' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 83, '01/08/2021', 'Blog' ); 
+INSERT INTO inscritos(cantidad, fecha, fuente) VALUES ( 99, '01/08/2021', 'Página' );
 
 -- Consultas del desafío:
 -- 1. ¿Cuántos registros hay? 
